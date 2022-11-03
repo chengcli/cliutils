8887b798ad5bbfe6106714d9b861206df4fc3dc1
Cheng Li <chengcli@umich.edu>
2022-11-03 17:41:25 -0400

    update source code

---
+++ cmake/setup_configure.cmake
@@ -0,0 +1,19 @@
+## set up model configuration ##
+
+set(CMAKE_BUILD_TYPE "DebugRelease")
+
+# configure athenapp
+message(STATUS "Include ${CMAKE_SOURCE_DIR}/athenapp/cmake/setup_configure.cmake")
+include(${CMAKE_SOURCE_DIR}/athenapp/cmake/setup_configure.cmake)
+
+if (CMAKE_BUILD_TYPE MATCHES "Debug")
+  if (NOT "DEBUG" IN_LIST BUILD_TYPES)
+    list(APPEND BUILD_TYPES "DEBUG")
+  endif()
+endif()
+
+if (CMAKE_BUILD_TYPE MATCHES "Release")
+  if (NOT "RELEASE" IN_LIST BUILD_TYPES)
+    list(APPEND BUILD_TYPES "RELEASE")
+  endif()
+endif()
